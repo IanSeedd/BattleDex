3cb7b360ec2024d18c2a93c116c136a66bbe4cfb
Dudss <Dudss2007@gmail.com>
2025-12-30 00:46:32 -0300

    projeto_beta

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.pokedex, name='pokedex'),
+    path('', views.home, name='home'),
+    path('pokedex/', views.pokedex, name='pokedex'),
+    path('pokedex/<int:pokemon_id>/', views.detalhe_pokemon, name='detalhe_pokemon'),
 ]
